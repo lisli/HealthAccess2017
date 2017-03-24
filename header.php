2ac9108c5dd4c352312ac949fa2ba79537f462ae
Lisa Yoder <lisa.alane.yoder@gmail.com>
2017-03-24 13:24:44 -0400

    adjust logo path, add image for page header test

--- header.php
+++ header.php
@@ -51,7 +51,7 @@
                 <?php dynamic_sidebar('top-nav-search'); ?>   
             </nav>     
 			<a href="<?php echo esc_url( home_url() ); ?>/" class="logo">
-				<img class="s_span_6" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" onerror="this.onerror=null; this.src='<?php echo get_template_directory_uri(); ?>/assets/images/logo.png'" alt="<?php bloginfo('name'); ?>">
+				<img class="s_span_6" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png'" alt="<?php bloginfo('name'); ?>">
 			</a>
             <?php wp_nav_menu( array( 'theme_location' => 'donate-link' ) ); ?>
 
