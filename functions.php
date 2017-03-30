7c32439c3b12f273a398ffd8a7adf6f8c49ef2d8
Lisa Yoder <lisa.alane.yoder@gmail.com>
2017-03-30 13:28:41 -0400

    update link to get involved in widget cta

--- functions.php
+++ functions.php
@@ -62,7 +62,7 @@ function health_access2017_widgets_init() {
 		'name' => __( 'Sidebar Top', 'health-access2017' ),
 		'id' => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '<span class="widget-cta"><a href="<a href="http://healthaccess.flywheelsites.com/get-involved/">View More Ways to Get Involved >></a></span></aside>',
+		'after_widget' => '<span class="widget-cta"><a href="http://healthaccess.flywheelsites.com/get-involved/">View More Ways to Get Involved >></a></span></aside>',
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 
